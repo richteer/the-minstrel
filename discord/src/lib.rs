94a29623afb4842d349e941977c6d4c27d8fbd61
Eric Richter <richteer@lastprime.net>
2022-05-30 12:46:08 -0500

    discord: purge the rest of the webserver remnants

--- discord/src/lib.rs
+++ discord/src/lib.rs
@@ -2,8 +2,6 @@ pub mod client;
 pub mod player;
 pub mod frontend;
 pub mod requester;
-// TODO: Disabled for now, web stuff will be split out in next commit (probably)
-//pub mod web;
 pub mod helpers;
 
 pub use client::MusicStateKey as MusicStateKey;
